6ae7f0a927388197ce110e694429ae7dcf2d84ea
Markus <markus@testmail.com>
2020-02-25 20:44:13 -0800

    updating E2E_Script2

--- TestProjectGit/src/Package1/E2E_Script2.java
+++ TestProjectGit/src/Package1/E2E_Script2.java
@@ -1,5 +1,29 @@
 package Package1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class E2E_Script2 {
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mark\\Desktop\\chrome\\chromedriver_win32\\chromedriver.exe");		
+
+WebDriver driver =new ChromeDriver();
+
+driver.get("http://spicejet.com"); //URL in the browser
+
+
+
+
+
+
+
+
+
+
+	}
 
 }
